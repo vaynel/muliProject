053c6b53bd1709a060c43775226d039f62859d39
yoon-hyuk <yoon960325@naver.com>
2023-03-24 17:25:56 +0900

    [yoon] board image ing

--- src/main/webapp/resources/assets/js/campingHome/boardForm.js
+++ src/main/webapp/resources/assets/js/campingHome/boardForm.js
@@ -6,49 +6,44 @@ const form = document.getElementById('form');
 form.addEventListener('submit', async (e)=>{
     e.preventDefault();
 
-    const formData = new FormData();
+
+    const formData = new FormData(form);
     const photos = document.querySelector('input[type="file"][multiple]');
+    //let title = document.getElementById('title').value;
+    //let text = document.getElementById('text').value;
+    //let hashtag = document.getElementById('hashtag').value;
 
-    formData.append('title','title');
-    formData.append('text','text');
-    formData.append('hashtag','hashtag');
-    for (let i = 0; i< photos.files.length; i++){
-        formData.append(`photos_${i}`,photos.files[i]);
-    }
+    //formData.append('title','yoon');
+    //formData.append('text','hyuk');
+    //formData.append('hashtag','hashtag');
+    formData.append(`files`, photos.files);
 
-
+    // for (let i = 0; i < photos.files.length; i++){
+    //     formData.append(`files`, photos.files);
+    // }
 
 
     // let data = {
     //     title: title.value,
     //     text: text.value,
     //     hashtag:hashtag.value,
-    //     photos_1:photos.files[1]
     // }
 
-
     await fetch("http://localhost:8080/campingHome/board/new",{
-        method : 'post',
+        method : 'POST',
         //redirect: 'follow',
         headers: {
             'header': header,
             'X-CSRF-Token': token,
-            'Content-Type': 'application/json',
-        },
-        //body: JSON.stringify(data)
-        body : JSON.stringify(formData)
-    })
-        // .then((response) => response.json())
-        // .then((result) => {
-        //     console.log('성공:', result);
-        // })
-        // .catch((error) => {
-        //     console.error('실패:', error);
-        // })
 
+        },
+        body : formData,
+    })
         .then((response)=>{
-            window.location.href = "http://localhost:8080/campingHome/boards?page=1";
-        }).catch((err)=>{
+            response.json()
+            //window.location.href = "http://localhost:8080/campingHome/boards?page=1";
+        }).then(result => console.log("Success:", result))
+        .catch((err)=>{
             console.log("err",err);
         })
 
